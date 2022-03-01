f875fcd2f057403b52b74044409a9a8b0c8e5644
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2022-03-01 08:25:25 +0100

    restructured responsibility

--- application.go
+++ application.go
@@ -28,9 +28,11 @@ func (app *application) indexHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	countVar := vars["count"]
+
+	// if count is not set or not a number
+	// we default to 10 words
 	count, err := strconv.Atoi(countVar)
 	if err != nil {
-		// defaults to 10 words
 		count = 10
 	}
 
@@ -41,39 +43,20 @@ func (app *application) indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Get the prefered format
 	acceptHeader := r.Header.Get("Accept")
-	if acceptHeader == "" {
-		acceptHeader = "application/json"
-	}
 
 	// Generate list of words
 	var words []string
 	for i := 0; i < count; i += 1 {
-		word := app.wordlist[randInt(0, len(app.wordlist)-1)]
+		randIdx := randInt(0, len(app.wordlist)-1)
+		word := app.wordlist[randIdx]
 		words = append(words, strings.Title(word))
 	}
 
 	resp := response{
-		Count: count,
-		Words: words,
+		Count:  count,
+		Words:  words,
+		Format: acceptHeader,
 	}
 
-	// Respond based on Accept header
-	switch acceptHeader {
-	case "text/html":
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write(resp.EncodePlain())
-	case "text/plain":
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write(resp.EncodePlain())
-	default:
-		data, err := resp.EncodeJSON()
-		if err != nil {
-			app.errorLog.Printf("could not build json response: %v", err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(data)
-	}
+	resp.WriteResponse(w)
 }
--- response.go
+++ response.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"encoding/json"
+	"net/http"
 	"strings"
 )
 
 type response struct {
-	Count int      `json:"count"`
-	Words []string `json:"words"`
+	Count  int      `json:"count"`
+	Words  []string `json:"words"`
+	Format string   `json:"-"`
 }
 
-func (r *response) EncodeJSON() ([]byte, error) {
-	return json.Marshal(r)
-}
+func (r *response) WriteResponse(w http.ResponseWriter) {
+	if r.Format == "text/plain" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(strings.Join(r.Words, "\n")))
+		return
+	}
 
-func (r *response) EncodePlain() []byte {
-	return []byte(strings.Join(r.Words, ", "))
+	// TODO: replace with html
+	if r.Format == "text/html" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(strings.Join(r.Words, ", ")))
+		return
+	}
+
+	// defaults to JSON
+	data, err := json.Marshal(r)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
